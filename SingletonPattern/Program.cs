1562273d3b6dd404657b3dd8c10ffd7b19bf8a46
ohnewdev <ohnewdev@gmail.com>
2021-04-03 14:41:32 +0900

    Add project files.

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public sealed class SingletonClass
+    {
+
+        private static SingletonClass _instance = null;
+        private static readonly object _lock = new object();
+
+        SingletonClass() { }
+        public static SingletonClass GetInstance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new SingletonClass();
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+}
